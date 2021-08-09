00dc8d3d695b6d1f8c0251990b51aa59bc96a404
Levan Apakidze <lapak18@freeuni.edu.ge>
2021-08-09 22:52:31 +0400

    firebase request sent

--- app/src/main/java/ge/edu/freeuni/messenger/app/LoginActivity.kt
+++ app/src/main/java/ge/edu/freeuni/messenger/app/LoginActivity.kt
@@ -2,10 +2,26 @@ package ge.edu.freeuni.messenger.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import ge.edu.freeuni.messenger.app.database.FirebaseUtil
+import ge.edu.freeuni.messenger.app.database.model.Message
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val ref = FirebaseUtil.ref
+
+        val et = findViewById<EditText>(R.id.edit_text_email)
+        val btn = findViewById<Button>(R.id.button_sign_in)
+
+        btn.setOnClickListener{
+            ref.child("mesg").setValue(Message(et.text.toString()))
+        }
+
     }
+
+
 }
